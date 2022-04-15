29d20fe32dffcbbd5b82bcc500c780ab909ccc51
NekoSong <nyankosong@gmail.com>
2022-04-15 16:39:50 +0800

    修正部分数学错误，更换部分超参数定义

--- perturb_func.m
+++ perturb_func.m
@@ -2,9 +2,9 @@ function ovar = proposal_func(ilgv, flag, var_mesh)
 %PROPOSAL_FUNC 建议分布函数
 % var 需要扰动的参数 flag 扰动参数类型（rho、z）
 
-% 对数扰动
 olgv = -1;
 while olgv <= 0
+    % 均匀分布方法，rho~U(-1,1)，z~U(-0.5,0.5)
     if strcmp(flag, 'rho')
         olgv = ilgv + 2*rand-1;
     elseif strcmp(flag, 'z')
@@ -14,3 +14,4 @@ while olgv <= 0
     ovar = mesh_func(olgv, var_mesh);
 end
 
+end
